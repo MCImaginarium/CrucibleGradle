b6f6f0460be28cf42c145327dcaaebb64b3584da
juanmuscaria <juanmuscaria@gmail.com>
2019-11-16 21:47:29 -0300

    Criado a base do projeto.

---
+++ src/main/java/io/github/cruciblemc/gradle/CruciblePlugin.java
@@ -0,0 +1,12 @@
+package io.github.cruciblemc.gradle;
+
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+
+public class CruciblePlugin implements Plugin<Project> {
+
+    @Override
+    public void apply(Project project) {
+
+    }
+}
